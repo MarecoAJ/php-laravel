3467a6c2c4a5b8be6e1a223d0fd3274aa13f5e55
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-10-03 20:25:09 -0300

    clase 13

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,6 +9,11 @@
     <p>
         <a href="{{ route('home') }}">Home</a>
         <a href="{{ route('blog') }}">Blog</a>
+        @auth
+            <a href="{{ route('dashboard') }}">Dashboard</a>
+        @else
+            <a href="{{ route('dashboard') }}">Login</a>
+        @endauth
     </p>
     <hr>
     @yield('content')
