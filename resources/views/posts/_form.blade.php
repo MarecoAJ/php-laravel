31993addeb668bba374379670b9ca3195acb6316
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-10-13 20:21:22 -0300

    clase 19

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,1 +1,19 @@
-Formulario
+@csrf
+<label class="uppercase text-gray-700 text-xs">Titulo</label>
+<input 
+type="text" 
+name="title" 
+class="rounded border-gray-200 w-full mb-4"
+value="{{ $post->title }}"
+>
+<lable class="uppercase text-gray-700 text-xs">contenido</lable>
+<textarea 
+name="body" 
+rows="5" 
+class="rounded border-gray-200 w-full mb-4" >
+{{ $post->body }}
+</textarea>
+<div class="flex justify-between items-center">
+    <a href="{{ route('posts.index') }}" class="text-indigo-500">Volver</a>
+    <input type="submit" value="Enviar" class="bg-gray-800 text-white rounded px-4 py-2">
+</div>
