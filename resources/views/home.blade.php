3074635fefaa0644bec447f9e0c38488ed1576c4
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-10-24 21:40:02 -0300

    clase 27

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,11 @@
 @extends('template')
 
 @section('content')
-    <div>
-
+    <div class="bg-gray-900 px20 py-16 rounded-lg mb-8 relative overflow-hidden">
+        <span class="text-xs uppercase text-gray-700 bg-gray-400 rounded-full px-2 py-1">Programacion</span>
+        <h1 class="text-3xl text-white mt-4">Blog</h1>
+        <p class="text-sm text-gray-400 mt-2">proyecto de desarrollo web</p>
+        <img src="{{ asset('images/dev.png') }}" class="absolute -right-20 top-0 opacity-20">
     </div>
     <div class="px-4">
         <h1 class="text-2xl mb-8 text-gray-900">Contenido tecnico</h1>
